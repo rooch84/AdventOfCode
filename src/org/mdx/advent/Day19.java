37f0f0373515e74f6e16c4b71dc18837e1637b17
Chris Rooney <c.rooney@mdx.ac.uk>
2016-12-19 15:51:39 +0000

    Day 19 - Tidy

--- src/org/mdx/advent/Day19.java
+++ src/org/mdx/advent/Day19.java
@@ -1,52 +1,70 @@
 package org.mdx.advent;
 
+/**
+ * Test input is 5
+ * Actual input is 3017957
+ * 
+ * --- Day 19: An Elephant Named Joseph ---
+ * 
+ * The Elves contact you over a highly secure emergency channel. Back at the
+ * North Pole, the Elves are busy misunderstanding White Elephant parties.
+ * 
+ * Each Elf brings a present. They all sit in a circle, numbered starting with
+ * position 1. Then, starting with the first Elf, they take turns stealing all
+ * the presents from the Elf to their left. An Elf with no presents is removed
+ * from the circle and does not take turns.
+ * 
+ * For example, with five Elves (numbered 1 to 5):
+ * 
+ * 1 5 2 4 3 Elf 1 takes Elf 2's present. Elf 2 has no presents and is skipped.
+ * Elf 3 takes Elf 4's present. Elf 4 has no presents and is also skipped. Elf 5
+ * takes Elf 1's two presents. Neither Elf 1 nor Elf 2 have any presents, so
+ * both are skipped. Elf 3 takes Elf 5's three presents. So, with five Elves,
+ * the Elf that sits starting in position 3 gets all the presents.
+ * 
+ * With the number of Elves given in your puzzle input, which Elf gets all the
+ * presents?
+ * 
+ * @author Chris Rooney
+ *
+ */
 public class Day19 {
 
 	public static void main(String[] args) {
-		int s = 3017957;
+		int s = Integer.parseInt(args[0]);
 		int[] e = new int[s];
 		for (int i = 0; i < s; ++i) {
 			e[i] = 1;
 		}
 		boolean oneE = false;
-		
+
 		int e1 = 0;
 		int e2 = 0;
 		while (!oneE) {
-			
-			
+
 			e2 = (e1 + 1) % s;
-			while (e[e2] == 0 && e2 != e1){
+			while (e[e2] == 0 && e2 != e1) {
 				e2 = (e2 + 1) % s;
-				//System.out.println("e2: " + e2 + ", e1: " + e1);
 			}
-		//	System.out.println("End of first loop");
-			
+
 			if (e2 == e1) {
 				oneE = true;
 				break;
 			}
-			
+
 			e[e1] += e[e2];
 			e[e2] = 0;
-			
-			
+
 			int eTmp = e1;
 			e1 = (e1 + 1) % s;
 			while (e[e1] == 0 && e1 != eTmp) {
 				e1 = (e1 + 1) % s;
-			//	System.out.println("Stuck in loop 2");
 			}
-			//System.out.println("Tmp is " + eTmp);
-				
-//			for (int i = 0; i < s; ++i) {
-//				System.out.println(i + " " + e[i]);
-//			}
 		}
-		
+
 		for (int i = 0; i < s; ++i) {
 			if (e[i] != 0) {
-				System.out.println(i + 1);				
+				System.out.println(i + 1);
 			}
 		}
 	}
